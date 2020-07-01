0ccb28d047c567ca30956ee48827346553976953
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-06-30 20:07:17 -0400

    added HTML, CSS and JS linked

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+var questions = [
+  {
+    question: "What is your name",
+    choices: ["a", "alksjfd", "as;dklj", "a;lskdj"],
+    correctAnswer: "alksjfd",
+  },
+  {
+    question: "What is your names",
+    choices: ["a", "alksjfd", "as;dklj", "a;lskdj"],
+    correctAnswer: "alksjfd",
+  },
+  {
+    question: "What is your city",
+    choices: ["a", "alksjfd", "as;dklj", "a;lskdj"],
+    correctAnswer: "alksjfd",
+  },
+  {
+    question: "What is your name",
+    choices: ["a", "alksjfd", "as;dklj", "a;lskdj"],
+    correctAnswer: "alksjfd",
+  },
+];
+//create counter to know which question you are on
+
+// var student = {
+//     firstName: "Ari",
+//     tutoring: true
+// }
+
+// var firstname = "Ari"
+// console.log(firstname)
+
+// console.log(student.firstName)
+// console.log(questions[3].question)
+// console.log(questions[3].choices[2])
