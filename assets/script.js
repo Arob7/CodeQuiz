590b67642307d165567eecdee53e70c7d7d518ed
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-07-03 17:58:12 -0400

    added JS questions, console logs, cards to HTML, loop

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,70 @@
-function Start()
+// function start() {
 
+// SHOULD START BUTTON BE ITS OWN FUNCTION APART FROM QUESTIONS OR TOGETHER??
 
-
-
+// THIS IS A VARIABLE THAT HAS ARRAYS WITHIN AN ARRAY AND MULTIPLE OBJECTS
 var questions = [
   {
-    question: "What does HTML stand for?",
+    question: "Which event occurs when the user clicks on an HTML element?",
+    choices: ["onchange", "onmouseclick", "onclick", "onmouseover"],
+    correctAnswer: "onclick",
+  },
+  {
+    question: "Inside which HTML tag do we put the JavaScript?",
+    choices: ["<js>", "<javascript>", "scripting", "<script>"],
+    correctAnswer: "<script>",
+  },
+  {
+    question: "How do you create a function in JavaScript?",
     choices: [
-      "Hyper Trainer Marking Language",
-      "Hyper Text Marketing Language",
-      "Hyper Text Marketing Language",
-      "Hyper Text Marketing Language",
+      "call aFunction()",
+      "aFunction()",
+      "call function aFunction",
+      "var aFunction()",
     ],
-    correctAnswer: "Hyper Text Markup Language",
+    correctAnswer: "aFunction()",
   },
   {
-    question: "What is your names",
-    choices: ["a", "alksjfd", "as;dklj", "a;lskdj"],
-    correctAnswer: "alksjfd",
+    question: "Which operator is used to assign a value to a variable?",
+    choices: ["*", "=", "-", ":"],
+    correctAnswer: "=",
   },
   {
-    question: "What is your city",
-    choices: ["a", "alksjfd", "as;dklj", "a;lskdj"],
-    correctAnswer: "alksjfd",
-  },
-  {
-    question: "What is your name",
-    choices: ["a", "alksjfd", "as;dklj", "a;lskdj"],
-    correctAnswer: "alksjfd",
+    question: "How to write an IF statement in JavaScript?",
+    choices: ["if i == 3", "if i = 3", "if (i==3)", "if (i=3) then"],
+    correctAnswer: "if (i==3)",
   },
 ];
+// }
+
+// PRACTICE AREA
+// console.log(questions[0].choices[2]);
+
+// console.log(questions[4].correctAnswer);
+
+// THIS LOOPS THROUGH THE QUESTIONS
+for (let i = 0; i < questions.length; i++) {
+  console.log(questions[i].question);
+}
+
+for (let x of questions) {
+  console.log(x);
+}
+
+// WILL I HAVE TO MAKE A LOOP FOR CHOICES AND CORRECTANSWER ALSO?
 
 //create counter to know which question you are on
-function Countdown() {
-  var timerInterval = setInterval(function () {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + " ";
+// function Countdown() {
+//   var timerInterval = setInterval(function () {
+//     secondsLeft--;
+//     timeEl.textContent = secondsLeft + " ";
 
-    if (secondsLeft === 0) {
-      clearInterval(timerInterval);
-      sendMessage();
-    }
-  }, 1000);
-}
+//     if (secondsLeft === 0) {
+//       clearInterval(timerInterval);
+//       sendMessage();
+//     }
+//   }, 1000);
+// }
 
 // var student = {
 //     firstName: "Ari",
