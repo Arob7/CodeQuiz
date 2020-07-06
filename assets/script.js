1563185608ed2a3381b5ae03f3242a8636b6a74c
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-07-06 17:26:34 -0400

    added timer, commented out radios, added answer buttons

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,97 @@
-// let startQuiz = document.getElementById("startButton");
+let startQuiz = document.getElementById("start-button");
+let questionContainerEl = document.getElementById("question-container");
+let questionEl = document.getElementById("question");
+let answerButtonEl = document.getElementById("answer-buttons");
+let submitButton = document.getElementById("submit-button");
 
-document.getElementById("startButton").addEventListener("click", start);
+// WHEN CLICKED THIS STARTS THE QUIZ
+startQuiz.addEventListener("click", start);
 
-//   document.getElementById("text").innerText
-// }
+// THIS IS THE COUNTDOWN TIMER
+var timeLeft = 15;
+var timeEl = document.querySelector(".time");
 
-// let answerTwo = "<script>";
-// let answerThree = "aFunction()";
-// let answerFour = "=";
-// let answerFive = "if (i==3)";
+// HOW TO GET START BUTTON INTO FUNCTION - PARAMETERS?
 
-function start() {
-  // document.getElementById("questionOne").innerHTML;
-  console.log(answer.value);
+function countDown() {
+  var timerInterval = setInterval(function () {
+    timeLeft--;
+    timeEl.textContent = timeLeft + " sec(s) left!";
+
+    if (timeLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+  }, 1000);
 }
-//   start.style.display = "none";
-// }
 
-let questionOne = [
+function sendMessage() {
+  timeEl.textContent = "Time's Up!";
+}
+
+countDown();
+
+// THIS IS THE FUNCTION THAT WILL START THE QUIZ
+function start() {
+  console.log("Quiz Started");
+  startQuiz.classList.add("hide");
+  questionContainerEl.classList.remove("hide");
+  submitButton.classList.remove("hide");
+  nextQuestion();
+}
+
+function nextQuestion() {}
+
+function showQuestion(questionS) {
+  questionEl.innerText = question.question;
+  question.choices.forEach((choices) => {
+    let button = document.createElement("button");
+    button.innerText = choices.text;
+    button.classList.add("choicebutton");
+    if (choices.correct) {
+      button.dataset.correct = choices.correct;
+    }
+  });
+}
+
+function submitAnswer() {}
+
+let questions = [
   {
     question: "Which event occurs when the user clicks on an HTML element?",
     choices: ["onchange", "onmouseclick", "onclick", "onmouseover"],
   },
+
+  {
+    question: "Inside which HTML tag do we put the JavaScript?",
+    choices: ["<js>", "<javascript>", "scripting", "<script>"],
+  },
+
+  {
+    question: "How do you create a function in JavaScript?",
+    choices: [
+      "call aFunction()",
+      "aFunction()",
+      "call function aFunction",
+      "var aFunction()",
+    ],
+  },
+
+  {
+    question: "Which operator is used to assign a value to a variable?",
+    choices: ["*", "=", "-", ":"],
+  },
+
+  {
+    question: "How to write an IF statement in JavaScript?",
+    choices: ["if i == 3", "if i = 3", "if (i==3)", "if (i=3) then"],
+  },
 ];
+
+// THIS LOOPS THROUGH THE QUESTIONS
+for (let i = 0; i < questions.length; i++) {
+  console.log(questions[i].question);
+}
 
 // THIS VALIDATES RADIO BUTTONS AND WORKS!!
 function check() {
@@ -30,6 +99,12 @@ function check() {
   else alert("Wrong answer!");
 }
 
+// let answerTwo = "<script>";
+// let answerThree = "aFunction()";
+// let answerFour = "=";
+// let answerFive = "if (i==3)";
+
+// QUESTIONS
 // let questionTwo = [
 //   {
 //     question: "Inside which HTML tag do we put the JavaScript?",
@@ -95,20 +170,7 @@ function check() {
 //   console.log(y)
 // }
 
-// WILL I HAVE TO MAKE A LOOP FOR CHOICES AND CORRECTANSWER ALSO?
-
 //create counter to know which question you are on --THIS IS TIMER
-// function Countdown() {
-//   var timerInterval = setInterval(function () {
-//     secondsLeft--;
-//     timeEl.textContent = secondsLeft + " ";
-
-//     if (secondsLeft === 0) {
-//       clearInterval(timerInterval);
-//       sendMessage();
-//     }
-//   }, 1000);
-// }
 
 // var student = {
 //     firstName: "Ari",
